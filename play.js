04f85b949a71e3d434346356eaddfd0f2c51f600
Matheson94 <mathesonchisholm@hotmail.com>
2022-01-19 22:04:39 +0000

    input.js made

--- play.js
+++ play.js
@@ -1,28 +1,9 @@
 const net = require("net");
 const { stdout } = require("process");
-const { connect } = require('./client.js')
+const { connect } = require('./client.js');
+const { setupInput } = require('./input.js');
 
 
-// setup interface to handle user input from stdin
-
-const setupInput = function(conn) {
-  connection = conn;
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', key => {
-    handleUserInput(key);
-  });
-  return stdin;
-};
-
-const handleUserInput = (key) => {
-  if (key === `\u0003`) {
-    stdout.write("Bye!");
-    process.exit();
-  }
-}
 
 console.log("Connecting ...");
 connect();
